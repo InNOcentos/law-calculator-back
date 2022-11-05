2c87669210655031504deb1546a185791b07e170
Boris Smirnov <bsmirnov@rvision.ru>
2022-11-05 20:08:28 +0300

    fix: jwt guards override default unauthorized exception

--- src/modules/common/guards/refresh-token.guard.ts
+++ src/modules/common/guards/refresh-token.guard.ts
@@ -1,5 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {}
+export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
+  override canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    return super.canActivate(context);
+  }
+
+  override handleRequest(err, user, info): any {
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user) {
+      throw err || new UnauthorizedException('Пользователь не авторизован');
+    }
+    return user;
+  }
+}
